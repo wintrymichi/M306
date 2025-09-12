f74e520e90e90b6aa4d59e88040a9172449b169c
michii :) <96500069+wintrymichi@users.noreply.github.com>
2025-09-12 15:23:58 +0200

    12.09.2025 - Vedi diario

--- 5_Applicativo/main.js
+++ 5_Applicativo/main.js
@@ -1,4 +1,12 @@
-function accessoMic() {
-    getUserMedia(Audio);
-    alert("Accesso al microfono consentito");
+debugToggle = false;
+function showDebug() {
+    debugDiv = document.getElementById("debugDiv");
+
+    if (debugToggle) {
+        debugDiv.style.display = "block";
+    }
+    else {
+        debugDiv.style.display = "none";
+    }
+    debugToggle =! debugToggle;
 }
